4ab2d479116cfe64751fbed8876a649d4a70ab3e
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-04-04 10:44:46 -0400

    a

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,12 @@
-import { colors } from '@ferraz-ui/tokens';
+import { styled } from './styles';
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+});
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Olá Mundo</h1>;
+  return <Button>Olá mundo</Button>;
 }
