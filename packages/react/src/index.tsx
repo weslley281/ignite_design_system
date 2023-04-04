b978f33d75403b7bb7169b49c904f905a33f157a
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2023-04-04 15:42:41 -0400

    a

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,8 @@
 import { styled } from './styles';
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
   borderRadius: '$md',
   padding: '$4',
 });
-
-export function App() {
-  return <Button>Olá mundo</Button>;
-}
